30ddaf9300db830c8d35fcbd70bf831424c9bbf5
KirillDavydov1 <82818161+KirillDavydov1@users.noreply.github.com>
2023-11-12 10:59:30 +0300

    12.11.2023

--- src/main/java/com/example/springbootcrud/controller/UserController.java
+++ src/main/java/com/example/springbootcrud/controller/UserController.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -21,10 +24,37 @@ public class UserController {
 
     @GetMapping("/users")
     public String findAll(Model model) {
-        List<User> userList = userService.findAll();
-        model.addAttribute("users", userList);
+        model.addAttribute("users", userService.findAll());
         return "userList";
     }
 
+    @GetMapping("/userCreate")
+    public String createUserForm(Model model) {
+        model.addAttribute("user", new User());
+        return "userCreate";
+    }
 
+    @PostMapping("/userCreate")
+    public String createUser(@ModelAttribute("user") User user) {
+        userService.save(user);
+        return "redirect:/users";
+    }
+
+    @GetMapping("/delete")
+    public String deleteUser(@RequestParam("id") Long id) {
+        userService.deleteById(id);
+        return "userList";
+    }
+
+    @GetMapping("/edit")
+    public String editUser(@RequestParam("id") Long id, Model model) {
+        model.addAttribute("user", userService.getById(id));
+        return "userUpdate";
+    }
+
+    @PostMapping("/userUpdate")
+    public String updateUser(@ModelAttribute("user") User user) {
+        userService.save(user);
+        return "redirect:/users";
+    }
 }
